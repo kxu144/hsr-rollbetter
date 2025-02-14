b7b67d0de264b2829bf8e2fb916c6aa427851885
Kevin Xu <kxu144@berkeley.edu>
2025-02-14 03:03:17 -0800

    timing

--- api-server/run.py
+++ api-server/run.py
@@ -1,5 +1,6 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+import time
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -23,11 +24,14 @@ def getData(uid):
 
 @app.route('/relics', methods=['POST'])
 def findProbability():
+    start_time = time.time()
     relics = request.get_json()['relics']
     
     results = executor.map(lambda t: p(*t), [(relic, {'CRIT Rate': 1, 'CRIT DMG': 1, 'ATK%': 2/3, 'ATK': 1/4}) for relic in relics])
     output = [{'mainstat': msp, 'substat': ssp, 'combinations': combs} for msp, ssp, combs in results]
 
+    end_time = time.time()
+    print(f"/relics Time Elapsed: {end_time - start_time}")
     return jsonify(output)
 
 if __name__ == '__main__':
